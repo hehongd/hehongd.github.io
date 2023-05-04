d0ec154d6aab6e18e6784cbc1560377b0308b2e0
何宏冬 <71446961+hehongd@users.noreply.github.com>
2023-05-04 10:30:42 +0800

    Update one.js

--- src/pages/one.js
+++ src/pages/one.js
@@ -1,1 +1,10 @@
-
+export default {
+    template: 
+    '<div>'+
+    '<h1>Vue3.x中的Mixin实现组件功能的复用 、全局配置Mixin</h1>'+
+    '<article class="articleContanier">'+
+        '<p>一、Vue3.x中的mixin介绍使用混入 (mixin) 提供了一种非常灵活的方式，来分发 Vue 组件中的可复用功能。一个混入对象可以包含任意组件选项。当组件使用混入对象时，所有混入对象的选项将被“混合”进入该组件本身的选项。</p>'+
+        '<p>1、新建mixin/base.js</p>'+
+    '</article>'
+    +'</div>'
+}
