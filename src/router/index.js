e1154570b75eddce6d2836cfd5f7362250a2310e
hehongd <635418133@qq.com>
2023-10-23 10:49:10 +0800

    212

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = []
+const routes = [
+  // {
+  //   path:'/404',
+  //   component:() => import('@/views/404'),
+  //   hidden:true
+  // }
+]
 
 const router = createRouter({
   history: createWebHistory(),
---
+++ src/views/404.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    访问的路径不存在     
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
